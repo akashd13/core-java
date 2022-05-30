e65ceb9e2627e6cc5cb9dbb0c2acd37401c5c63c
akashd13 <akash13.xworkz@gmail.com>
2022-05-30 21:57:45 +0530

    Multilevel inheritance program

---
+++ core_java/src/com/xworkz/core_java/Inheritance/Multilevel_inheritance/Bajaj.java
@@ -0,0 +1,17 @@
+package com.xworkz.core_java.Inheritance.Multilevel_inheritance;
+
+public class Bajaj extends Motorcycle {
+	
+	String CompanyName = "Bajaj";
+	
+	void BajajFuelType() {
+		
+		System.out.println("All bajaj bikes use " + Fuel);
+	}
+	
+	void NameofCompany() {
+		
+		System.out.println("Name of the company is " + CompanyName);
+	}
+
+}
